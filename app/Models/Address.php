fe38b503819b5146c2b6e7cc307021819a21b943
HảiLamdz <vuhailam2112@gmail.com>
2025-05-19 18:25:43 +0700

    Change address model

---
+++ app/Models/Address.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'recipient_name',
+        'phone',
+        'address_detail',
+        'city',
+        'district',
+        'ward',
+        'is_default'
+    ];
+
+    protected $casts = [
+        'is_default' => 'boolean'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public $incrementing = false; 
+    protected $keyType = 'string';
+}
