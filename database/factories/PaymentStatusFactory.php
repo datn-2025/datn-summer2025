d92b72a521965a030751cf7eabf245eee7430ebf
HảiLamdz <vuhailam2112@gmail.com>
2025-05-19 23:08:19 +0700

    Change PaymentStatus Factory And Seeder

--- database/factories/PaymentStatusFactory.php
+++ database/factories/PaymentStatusFactory.php
@@ -2,27 +2,22 @@
 
 namespace Database\Factories;
 
+use App\Models\PaymentStatus;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PaymentStatus>
- */
 class PaymentStatusFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = PaymentStatus::class;
+
     public function definition(): array
     {
-         $statuses = [
-            ['name' => 'Đang Chờ Xử Lý', 'description' => 'Thanh toán đang chờ xử lý'],
-            ['name' => 'Chưa Thanh Toán', 'description' => 'Chưa Thanh Toán'],
-            ['name' => 'Thanh Toán Thành Công', 'description' => 'Thanh toán thành công'],
-            ['name' => 'Thanh toán thất bại', 'description' => 'Thanh toán thất bại'],
+        return [
+            'name' => $this->faker->unique()->randomElement([
+                'Chờ Xử Lý',
+                'Chưa thanh toán',
+                'Đã Thanh Toán',
+                'Thất Bại',
+            ])
         ];
-
-        return $this->faker->unique()->randomElement($statuses);
     }
 }
