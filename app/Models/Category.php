502bdeb516e5114e4e2f9b3d2bbfdb6b5efe8f96
HảiLamdz <vuhailam2112@gmail.com>
2025-05-19 22:51:22 +0700

    Change Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,17 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
-
 class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'name',
-        'slug',
-        'parent_id',
-    ];
+    protected $fillable = ['name', 'slug', 'image'];
+
+    public $incrementing = false; 
+    protected $keyType = 'string';
+
+    public function books(): HasMany
+    {
+        return $this->hasMany(Book::class);
+    }
 
     protected static function boot()
     {
@@ -25,26 +29,5 @@ class Category extends Model
                 $model->id = (string) Str::uuid();
             }
         });
-    }
-
-    public $incrementing = false;
-    protected $keyType = 'string';
-
-    // Danh mục cha
-    public function parent()
-    {
-        return $this->belongsTo(Category::class, 'parent_id');
-    }
-
-    // Danh mục con
-    public function children()
-    {
-        return $this->hasMany(Category::class, 'parent_id');
-    }
-
-    // Sản phẩm thuộc danh mục
-    public function products()
-    {
-        return $this->hasMany(Product::class);
     }
 }
