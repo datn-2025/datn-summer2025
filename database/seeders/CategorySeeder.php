46d4d15bce2904cff8daabec528a1afa221abb75
HảiLamdz <vuhailam2112@gmail.com>
2025-05-19 23:06:10 +0700

    Change Category Factory And Seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,24 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        // Tạo 5 danh mục cha
-        $parents = Category::factory()->count(5)->create();
-
-        // Tạo 2 danh mục con cho mỗi danh mục cha
-        // foreach ($parents as $parent) {
-        //     Category::factory()->count(2)->create([
-        //         'parent_id' => $parent->id
-        //     ]);
-        // }
+        Category::factory(10)->create();
     }
 }
