fc455ee8b9a8027e0e2fc656c753d6474cf15438
HảiLamdz <vuhailam2112@gmail.com>
2025-05-19 22:49:10 +0700

    Change Attribute model

---
+++ app/Models/Attribute.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Attribute extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function values(): HasMany
+    {
+        return $this->hasMany(AttributeValue::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid();
+            }
+        });
+    }
+
+    public $incrementing = false; 
+    protected $keyType = 'string';
+}
