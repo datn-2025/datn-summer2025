44a3cf754def3ea5c80812fe58a5c968db86a239
Đặng Duy Tiến <tienddph50842@gmail.com>
2025-05-21 17:19:19 +0700

    update danh sach san pham , category , sreach , them cac file js , css o public

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BookController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+// Route có tham số categoryId là optional (có hoặc không)
+Route::get('/books/{categoryId?}', [BookController::class, 'index'])->name('books.index');
+?>
