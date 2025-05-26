529f5faf76486dca8894475035bf1b02e2e28ce2
vanvan9825 <tvan19525@gmail.com>
2025-05-26 16:08:49 +0700

    feat: cài thông báo toastrtoastr

--- resources/views/account/login.blade.php
+++ resources/views/account/login.blade.php
@@ -155,4 +155,11 @@
         background-color: #a83228;
     }
 </style>
+<!-- Toastr -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet"/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+{!! Toastr::message() !!}
+
 @endsection
