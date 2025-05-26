eee3f70e5bf07d428c7c296841d562c0d0de76f0
HảiLamdz <vuhailam2112@gmail.com>
2025-05-26 23:37:04 +0700

    change product voucher table

--- database/migrations/2025_05_22_133106_create_product_voucher_table.php
+++ database/migrations/2025_05_22_133106_create_product_voucher_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
     $table->uuid('product_id');
 
     $table->foreign('voucher_id')->references('id')->on('vouchers')->onDelete('cascade');
-    $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+    $table->foreign('product_id')->references('id')->on('books')->onDelete('cascade');
 });
 
     }
