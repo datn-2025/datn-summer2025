320474b65dd436cf2ccca04285f5eb596b33808f
TuyetChi26 <tuyetchi261@gmail.com>
2025-06-06 15:00:58 +0700

    fix; bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => AdminMiddleware::class,
         ]);
-
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
