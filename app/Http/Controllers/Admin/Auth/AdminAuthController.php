b35c65cf394e0a73c60207a8d89f3fce992ee700
TuyetChi26 <tuyetchi261@gmail.com>
2025-06-06 14:34:30 +0700

    fix: admin auth controller

--- app/Http/Controllers/Admin/Auth/AdminAuthController.php
+++ app/Http/Controllers/Admin/Auth/AdminAuthController.php
@@ -2,16 +2,9 @@
 
 namespace App\Http\Controllers\Admin\Auth;
 
-use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Toastr;
-use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\ValidationException;
 
 class AdminAuthController extends Controller
 {
@@ -64,21 +57,6 @@ class AdminAuthController extends Controller
             'email' => 'Email hoặc mật khẩu không chính xác',
         ]);
 
-        // $user = User::with('role')->where('email', $request->email)->first();
-
-        // // if (!$user|| $user->role_id != "702e2e4d-ae16-31eb-81c8-8224d58e9e9f") {
-        // if (!$user|| $user->role->name != "Admin") {
-        //    return back()->withErrors([
-        //         'email' => ['Bạn không có quyền truy cập vào trang quản trị'],
-        //     ]);
-        // }
-
-        // if ($user->status != "Hoạt Động") {
-        //    return back()->withErrors([
-        //         'email' => ['Tài khoản của bạn đã bị khóa hoặc chưa được kích hoạt.'],
-        //     ]);
-        // }
-
         // if (!Hash::check($request->password, $user->password)) {
         //    return back()->withErrors([
         //         'email' => ['Email không chính xác.'],
@@ -86,21 +64,15 @@ class AdminAuthController extends Controller
         //     ]);
         // }
 
-        // $request->session()->regenerate();
         // $request->session()->put([
         //     'admin_id' => $user->id,
         //     'admin_name' => $user->name,
         //     'admin_role' => $user->role->name
         // ]);
-        // return redirect()->intended(route('admin.dashboard'));
     }
 
     public function logout(Request $request)
     {
-        // $request->session()->invalidate();
-        // $request->session()->regenerateToken();
-
-        // return redirect()->route('admin.login');
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
