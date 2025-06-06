3f2f55faeedf54726b9b8f33158f8956b38e6bf2
TuyetChi26 <tuyetchi261@gmail.com>
2025-06-06 14:37:03 +0700

    fix: admin auth middleware

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -5,17 +5,11 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-// use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Session;
 
 class AdminMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        // if (!Session::has('admin_id')) {
-        //     return redirect()->route('admin.login');
-        // }
-
         if (!Auth::check()) {
             return redirect()->route('admin.login')
                 ->with('error', 'Bạn chưa đăng nhập');
