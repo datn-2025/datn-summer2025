68ee1a1f1cf6ae8c0f6031e9f0904b476fc06337
nguyen doan hoang <nguyendoanhoang4@gmail.com>
2025-06-08 16:21:48 +0700

    Update book author chart component view

--- resources/views/livewire/book-author-chart.blade.php
+++ resources/views/livewire/book-author-chart.blade.php
@@ -6,7 +6,9 @@
 
 @push('scripts')
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
 <script>
+    Chart.register(ChartDataLabels);
     const ctxAuth = document.getElementById('authorChart').getContext('2d');
     new Chart(ctxAuth, {
         type: 'pie',
@@ -22,6 +24,24 @@
             plugins: {
                 legend: {
                     position: 'bottom'
+                },
+                datalabels: {
+                    display: true,
+                    color: '#fff',
+                    formatter: (value, ctx) => {
+                        const total = ctx.chart.data.datasets[0].data.reduce((a, b) => a + b, 0);
+                        if (total === 0) {
+                            return '';
+                        }
+                        const percentage = ((value / total) * 100).toFixed(1);
+                        return `${percentage}%`;
+                    },
+                    anchor: 'center',
+                    align: 'center',
+                    font: {
+                        weight: 'bold',
+                        size: 12
+                    }
                 }
             }
         }
