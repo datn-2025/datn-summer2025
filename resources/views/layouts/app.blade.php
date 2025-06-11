f707027fe4e838f9506f9e9fbe209f307e8dde63
Đặng Duy Tiến <tienddph50842@gmail.com>
2025-06-11 22:04:21 +0700

    tiến update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,16 +8,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>@yield('title', 'Trang tài khoản')</title>
 
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
 
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet" />
 
     <!-- Toastr CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet" />
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
     @stack('styles')
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -36,6 +39,9 @@
             document.write('<script src="https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver"><\/script>');
         }
     </script>
+<<<<<<< HEAD
+>>>>>>> 4210c9f2eaa61e640c56446bf28097f8faa0fc85
+=======
 
     <!-- Google Fonts Roboto -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
@@ -73,6 +79,7 @@
         background-clip: text;
       }
     </style>
+>>>>>>> c9677efe0cb9a144a1aec506cd4fbbac9a7d2a12
 </head>
 
 <body style="margin:0; min-height:100vh;">
