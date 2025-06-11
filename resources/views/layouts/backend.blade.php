2768454307f114a8067f8ae2c3560ad97bd07aed
TuyetChi26 <tuyetchi261@gmail.com>
2025-06-11 21:01:53 +0700

    Merge branch 'master' into fix/admin-login-logout

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -724,8 +724,9 @@
                     <ul class="navbar-nav" id="navbar-nav">
                         <li class="menu-title"><span data-key="t-menu">Menu</span></li>
                         <li class="nav-item">
-                            <a class="nav-link menu-link" href="{{ route('admin.dashboard') }}">
-                                <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Báo cáo tổng quan</span>
+                            <a class="nav-link menu-link" href="{{route('admin.dashboard')}}">
+                                <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Báo cáo tổng
+                                    quan</span>
                             </a>
                         </li> <!-- end Dashboard Menu -->
 
