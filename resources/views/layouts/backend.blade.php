cbc1127ab8ebac1fb267e8bbe34d344ae94cfda3
TuyetChi26 <tuyetchi261@gmail.com>
2025-06-12 17:22:08 +0700

    sửa logo

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -372,21 +372,21 @@
             <!-- LOGO -->
             <div class="navbar-brand-box">
                 <!-- Dark Logo-->
-                <a href="index.html" class="logo logo-dark">
+                <a href="{{ route('admin.dashboard') }}" class="logo logo-dark">
                     <span class="logo-sm">
-                        <img src="{{ asset('assets/images/logo-sm.png') }}" alt="" height="22">
+                        <img src="/images/logo-admin-layout-login.png" alt="" height="22">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ asset('assets/images/logo-dark.png') }}" alt="" height="17">
+                        <img src="/images/logo-admin-layout-login.png" alt="" height="17">
                     </span>
                 </a>
                 <!-- Light Logo-->
                 <a href="{{ route('admin.dashboard') }}" class="logo logo-light">
                     <span class="logo-sm">
-                        <img src="{{ asset('assets/images/logo-sm.png') }}" alt="" height="22">
+                        <img src="/images/logo-admin-layout-login.png" alt="" height="42">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ asset('assets/images/logo-light.png') }}" alt="" height="17">
+                        <img src="/images/logo-admin-layout-login.png" alt="" height="37">
                     </span>
                 </a>
                 <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
