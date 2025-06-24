ec1e8449f5794461dfa085d02da6767d2742e642
nguyen doan hoang <nguyendoanhoang4@gmail.com>
2025-06-25 00:47:32 +0700

    Update BookBrandChart component

--- app/Livewire/BookBrandChart.php
+++ app/Livewire/BookBrandChart.php
@@ -11,8 +11,32 @@ class BookBrandChart extends Component
 
     public function mount()
     {
-        $this->brandStats = Brand::withCount('books')->get();
+        $rawStats = Brand::withCount('books')->get();
+
+        $topN = 6; // Hiển thị Top 6 thương hiệu
+        $sorted = $rawStats->sortByDesc('books_count');
+
+        $topBrands = $sorted->take($topN)->map(function ($brand) {
+            return [
+                'name' => $brand->name,
+                'books_count' => $brand->books_count
+            ];
+        });
+
+        $otherTotal = $sorted->skip($topN)->sum('books_count');
+
+        $finalStats = $topBrands->values();
+
+        if ($otherTotal > 0) {
+            $finalStats->push([
+                'name' => 'Khác',
+                'books_count' => $otherTotal
+            ]);
+        }
+
+        $this->brandStats = $finalStats;
     }
+
     public function render()
     {
         return view('livewire.book-brand-chart');
