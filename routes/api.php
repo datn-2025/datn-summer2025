0f48e4ebbb4a27c73140a04e4a55e34fb4dead04
vanvan9825 <tvan19525@gmail.com>
2025-06-25 20:19:31 +0700

    feat: route api

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\MessageController;
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/messages', [MessageController::class, 'index']);
+    Route::post('/messages', [MessageController::class, 'store']);
+    Route::apiResource('/messages',MessageController::class);
+});
+
+
