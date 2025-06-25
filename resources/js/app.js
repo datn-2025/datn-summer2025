05218dd1d0b25196d2db0b2df783f7456bd24436
vanvan9825 <tvan19525@gmail.com>
2025-06-25 20:22:07 +0700

    feat: impot vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,11 @@ import './description-toggle';
 import './collection-swiper';
 import './review-swiper';
 import './quantity';
+import { createApp } from 'vue';
+import ChatWidget from './components/ChatWidget.vue';
+
+const app = createApp({});
+
+app.component('chat-widget', ChatWidget);
+
+app.mount('#app');
